213f6cc764cd296365f423ffd10095e825ef9dc7
Matthias Vogel <m.vogel@andersundsehr.com>
2018-12-18 09:37:07 +0100

    [TASK] add more test cases

--- tests/Alpha/TheInterfaceTest.php
+++ tests/Alpha/TheInterfaceTest.php
@@ -24,10 +24,13 @@ class TheInterfaceTest extends TestCase
         $global->databases->main = 'mysql://user:pw@host:3307/database';
         $global->sshConnections = new SshConnections();
         $global->sshConnections->addConnection((new SshConnection())->setHost('serverEu')->setUrl('user@server.eu')->setIdentityFile('docker/testCaseD/id_rsa'));
+        $global->sshConnections->addConnection((new SshConnection())->setHost('stagingServer')->setUrl('staging@stagingServer.server.eu')->setIdentityFile('docker/testCaseD/id_rsa'));
         $global->appInstances = new \stdClass();
         $global->appInstances->production = (new AppInstance())->setName('production')->setHost('serverEu')->setPath('/var/www/production');
+        $global->appInstances->staging = (new AppInstance())->setName('staging')->setHost('stagingServer')->setPath('/var/www/staging');
         $global->appInstances->testing = (new AppInstance())->setName('testing')->setHost('serverEu')->setPath('/var/www/testing');
         $global->appInstances->local = (new AppInstance())->setName('local')->setHost('')->setPath('./');
+        $global->appInstances->local2 = (new AppInstance())->setName('local2')->setHost('')->setPath('../local2');
         $result = $interface->getCommands($global, 'production', 'local', '');
         $this->assertSame([
             'rsync -e "ssh -F .phpsu/config/ssh_config" serverEu:/var/www/production/fileadmin/* ./fileadmin/',
@@ -39,6 +42,18 @@ class TheInterfaceTest extends TestCase
             'ssh -F .phpsu/config/ssh_config serverEu -C "rsync /var/www/production/fileadmin/* /var/www/testing/fileadmin/"',
             'ssh -F .phpsu/config/ssh_config serverEu -C "rsync /var/www/production/uploads/* /var/www/testing/uploads/"',
             'ssh -F .phpsu/config/ssh_config serverEu -C "mysqldump -hhost -P3307 -uuser -ppw database | mysql -hhost -P3307 -uuser -ppw database"',
+        ], $result);
+        $result = $interface->getCommands($global, 'local', 'local2', '');
+        $this->assertSame([
+            'rsync ./fileadmin/* ../local2/fileadmin/',
+            'rsync ./uploads/* ../local2/uploads/',
+            'mysqldump -hhost -P3307 -uuser -ppw database | mysql -hhost -P3307 -uuser -ppw database',
+        ], $result);
+        $result = $interface->getCommands($global, 'production', 'staging', '');
+        $this->assertSame([
+            'rsync -e "ssh -F .phpsu/config/ssh_config" serverEu:/var/www/production/fileadmin/* stagingServer:/var/www/staging/fileadmin/',
+            'rsync -e "ssh -F .phpsu/config/ssh_config" serverEu:/var/www/production/uploads/* stagingServer:/var/www/staging/uploads/',
+            'ssh -F .phpsu/config/ssh_config serverEu -C "mysqldump -hhost -P3307 -uuser -ppw database" | ssh -F .phpsu/config/ssh_config stagingServer -C "mysql -hhost -P3307 -uuser -ppw database"',
         ], $result);
     }
 }
