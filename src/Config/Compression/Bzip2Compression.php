0cda06b39ba53e534131cc630c715c60bd4fa307
Matthias Vogel <m.vogel@andersundsehr.com>
2019-08-02 13:18:14 +0000

    :sparkles: #61 add compression to database

---
+++ src/Config/Compression/Bzip2Compression.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPSu\Config\Compression;
+
+final class Bzip2Compression implements CompressionInterface
+{
+    public function getCompressCommand(): string
+    {
+        return ' | bzip2';
+    }
+
+    public function getUnCompressCommand(): string
+    {
+        return 'bunzip2 | ';
+    }
+}
