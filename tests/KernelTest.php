d1d9c51d4fec1fc5ce155b7bc407c1fbbe47304e
Christian Rodriguez Benthake <c.benthake@ecentral.de>
2021-07-02 00:36:33 +0200

    fix test

--- tests/KernelTest.php
+++ tests/KernelTest.php
@@ -8,6 +8,9 @@ use PHPSu\Cli\InfoCliCommand;
 use PHPSu\Cli\MysqlCliCommand;
 use PHPSu\Cli\SshCliCommand;
 use PHPSu\Cli\SyncCliCommand;
+use PHPSu\Config\ConfigurationLoader;
+use PHPSu\Config\ConfigurationLoaderInterface;
+use PHPSu\Config\GlobalConfig;
 use PHPSu\Kernel;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Application;
@@ -16,6 +19,13 @@ final class KernelTest extends TestCase
 {
     public function testPhpsuApplicationCommand(): void
     {
+        $configurationLoader = new ConfigurationLoader();
+        $reflection = new \ReflectionObject($configurationLoader);
+        $property = $reflection->getProperty('config');
+        $property->setAccessible(true);
+        $property->setValue($configurationLoader, new GlobalConfig());
+
+        Kernel::getContainer()->set(ConfigurationLoaderInterface::class, $configurationLoader);
         $app = Kernel::getContainer()->get(Application::class);
         $this->assertInstanceOf(SyncCliCommand::class, $app->get('sync'));
         $this->assertInstanceOf(SshCliCommand::class, $app->get('ssh'));
